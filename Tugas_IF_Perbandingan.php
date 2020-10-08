b9b6f7259a5c4955c5dc3fd2b780136b03091d5d
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 12:03:03 +0700

    Add files via upload

---
+++ Tugas_IF_Perbandingan.php
@@ -0,0 +1,34 @@
+<?php
+$a = 10;
+$b = 10;
+
+if ($a > $b){
+  echo "$a lebih besar dari $b <br>";
+}else if ($a  <$b){
+  echo "$a lebih kecil dari $b <br>";
+}else{
+  echo "$a sama dengan $b <br>";
+}
+
+$a = 4;
+$b = 7;
+
+if ($a > $b){
+  echo "$a lebih besar dari $b <br>";
+}else if ($a  <$b){
+  echo "$a lebih kecil dari $b <br>";
+}else{
+  echo "$a sama dengan $b <br>";
+}
+
+$a = 6;
+$b = 2;
+
+if ($a > $b){
+  echo "$a lebih besar dari $b <br>";
+}else if ($a  <$b){
+  echo "$a lebih kecil dari $b <br>";
+}else{
+  echo "$a sama dengan $b <br>";
+}
+?>
